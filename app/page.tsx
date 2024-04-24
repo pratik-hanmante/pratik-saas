1dcfd03ed6c7ae5becf1b6172b531d809cf91b26
pratik-hanmante <pratikhanmante3@gmail.com>
2024-04-25 04:01:30 +0530

    landing page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
 import { ModeToggle } from "@/components/ModeToggle";
-
+import { Button } from "@/components/ui/button";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-   <div>
-   
-    < ModeToggle />
-   </div>
-  )
+    <div>
+      <section className="flex items-center justify-center bg-background h-[90vh]">
+        <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
+          <div className="max-w-3xl mx-auto text-center">
+            <div>
+              <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+                <span>Sort your notes easily</span>
+              </span>
+              <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
+                Create your notes here
+              </h1>
+              <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+                quos quam quis eligendi doloribus nostrum! Aliquid error nam
+                magni! Atque suscipit delectus necessitatibus.
+              </p>
+            </div>
 
+            <div className="flex justify-center max-w-sm mx-auto mt-10">
+              <RegisterLink>
+                <Button size="lg" className="w-full">
+                  Sign Up for free
+                </Button>
+              </RegisterLink>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
-   
